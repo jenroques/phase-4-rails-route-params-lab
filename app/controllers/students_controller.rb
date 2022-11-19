d4f075b2716e13059493623ae2403786a6bd4f78
Jenny Roques <90016446+jenroques@users.noreply.github.com>
2022-11-19 09:21:30 -0600

    commit tree c459a04826b37e68bf4f6e5920defac62fdfa397

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,11 @@
 class StudentsController < ApplicationController
-
   def index
     students = Student.all
     render json: students
   end
 
+  def show
+    students_id = Student.find(params[:id])
+    render json: students_id
+  end
 end
